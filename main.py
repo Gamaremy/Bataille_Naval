0f05b66c0049728b45f1f9e3dce407073a48f96f
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2023-11-17 15:51:23 +0100

    Création des methodes de la classe Bateau
    Création de la fonction :
    -Get x et Get y
    - Element pour
    - Est coulé,

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from Bateau import Bateau
 
 
 def main():
-    test=Bateau()
-    grille=test.grille()
-    print(grille)
+    test=Bateau(5,5,5,False)
+    print(test.get_x())
+    print(test.get_y())
+    print(test.element())
+    print(test.est_coule())
+    
+
+
+### Initialisation test sur 
 
 
     
