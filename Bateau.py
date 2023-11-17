6c06b5597a2bcaeb8fb40e2b4891eb320124bccf
wiambekrigema <wiambekrigema@github.com>
2023-11-17 15:40:37 +0100

    ajouter la fonction est_touche
    elle retourne true si le bateau a touché la case.

--- Bateau.py
+++ Bateau.py
@@ -8,7 +8,16 @@ class Bateau:
         grillexy = [[0] * 10 for _ in range(9)]
         return(grillexy)
         
-        
+    def est_touche(self, x, y):
+       if self.horizontal:
+           for i in range(self.taille):
+               if self.cordonneex + i == x and self.cordonneey == y:
+                  return True
+       else:
+           for i in range(self.taille):
+               if self.cordonneex == x and self.cordonneey + i == y:
+                  return True
+       return False
         
 
 
